e1b9b29101d887ed97ab6aea8ea626ec52c8927d
Kevin Kanyingi <kevin.kanyingi@icloud.com>
2020-05-01 11:40:33 +0100

    added check for current run id

--- src/index.ts
+++ src/index.ts
@@ -11,14 +11,20 @@ if (!core) {
 
 async function main() {
   const { eventName, sha, ref, repo: { owner, repo }, payload } = github.context;
+  let actionRunId = process.env.GITHUB_RUN_ID;
   let branch = ref.slice(11);
   let headSha = sha;
+  let currentRunId: number | undefined;
   if (payload.pull_request) {
     branch = payload.pull_request.head.ref;
     headSha = payload.pull_request.head.sha;
   }
 
-  console.log({ eventName, sha, headSha, branch, owner, repo });
+  if(actionRunId) {
+    currentRunId = parseInt(actionRunId);
+  }
+
+  console.log({ eventName, sha, headSha, branch, owner, repo, currentRunId });
   const workflow_id = core.getInput('workflow_id', { required: true });
   const workflow_ids = workflow_id.replace(/\s/g, '').split(',').map(s => Number(s));
   const token = core.getInput('access_token', { required: true });
@@ -36,7 +42,7 @@ async function main() {
       });
       console.log(`Found ${data.total_count} runs total.`);
       const runningWorkflows = data.workflow_runs.filter(
-        workflow => workflow.status !== 'completed'
+        workflow => workflow.id != currentRunId && workflow.status !== 'completed'
       );
       console.log(`Found ${runningWorkflows.length} runs in progress.`);
       for (const {id, head_sha, status} of runningWorkflows) {
